ca7186f35de00d797be65261a439b3b09f73a54d
Vinith1697 <97vinith97@gmail.com>
2022-04-08 23:09:33 -0400

    Final commit

---
+++ To-Do-App/To-Do-App/Pages/AddToDo.cshtml.cs
@@ -0,0 +1,77 @@
+/*
+ * Razor page to add a new todo
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using To_Do_App.Model;
+using To_Do_App.Services;
+
+namespace To_Do_App.Pages
+{
+
+    public class AddToDoModel: PageModel
+    {
+        public ToDoModel ToDoItem = new();
+        private ToDoService _service;
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Task { get; set; } = string.Empty;
+        public string Descrip { get; set; }
+
+        public AddToDoModel(ToDoService service)
+        {
+            _service = service;
+        }
+
+        //Fill the field if user came from edit button
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id != null)
+            {
+                List<ToDoModel> items = _service.GetUncompleteToDos();
+
+                ToDoItem = items.FirstOrDefault(m => m.Id == id);
+
+                Task = ToDoItem.Task;
+                Descrip = ToDoItem.Descrip;
+            }
+
+            return Page();
+        }
+
+
+        //Called on Submit to button to create a new todo or edit a todo
+        public IActionResult OnPostSubmit(ToDoModel model)
+        {
+            if (model.Task == null)
+            {
+                return null;
+            }
+
+            if (model.Id == null)
+            {
+                model.Id = _service.GetUncompleteToDos().Count + 1;
+                model.IsDone = false;
+                _service.AddToDo(model);
+            }
+            else
+            {
+                List<ToDoModel> items = _service.GetUncompleteToDos();
+                int index = items.FindIndex(m => m.Id == model.Id);
+                _service.AddToDoAt(model, index);
+            }
+
+            return RedirectToPage("Home");
+
+        }
+
+    }
+}
---
+++ To-Do-App/To-Do-App/Services/ToDoService.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Service class to manage the TODO model;
+ */
+
+
+
+
+using System;
+using System.Collections.Generic;
+using To_Do_App.Model;
+
+namespace To_Do_App.Services
+{
+    public class ToDoService
+    {
+        List<ToDoModel> ToDoList = new();
+        List<ToDoModel> RemToDoList = new();
+
+        //Get list of rem todos
+        public List<ToDoModel> GetUncompleteToDos()
+        {
+            return RemToDoList;
+        }
+
+        //Add a new todo to the list
+        public List<ToDoModel> AddToDo(ToDoModel model)
+        {
+            ToDoList.Add(model);
+            return ToDoList;
+        }
+
+        //Replace a todo
+        public void AddToDoAt(ToDoModel model, int index)
+        {
+            ToDoList.RemoveAt(index);
+            ToDoList.Insert(index, model);
+        }
+
+        //Remove a todo
+        public List<ToDoModel> RemoveToDo(int id)
+        {
+            int ItemIndex = 0;
+
+            for (int i = 0; i < ToDoList.Count; i++)
+            {
+                if (ToDoList[i].Id == id)
+                {
+                    ItemIndex = i;
+                }
+            }
+
+            ToDoList.RemoveAt(index: ItemIndex);
+            return ToDoList;
+        }
+
+        //Mark a todo as completed
+        public List<ToDoModel> MarkAsDone(ToDoModel model)
+        {
+            model.IsDone = true;
+            model.CompletionDate = DateTime.Now;
+            int index = RemToDoList.FindIndex(m => m.Id == model.Id);
+            RemToDoList.RemoveAt(index);
+
+            return RemToDoList;
+        }
+
+        //Constructor with 3 default todos
+        public ToDoService()
+        {
+            //Sample tasks
+            ToDoList.Add(new ToDoModel { Id = 1, Task = "Cook dinner", Descrip = "Scrambled Eggs and chicken poulet", IsDone = false });
+            ToDoList.Add(new ToDoModel { Id = 2, Task = "Watch lecture recording", Descrip = "Friday 12 pm lecture", IsDone = false });
+            
+
+            RemToDoList = ToDoList;
+        }
+    }
+}
